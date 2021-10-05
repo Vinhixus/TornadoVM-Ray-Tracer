b686eecd73b6f76c767974d1c1dc76d552abffcd
Vinh Pham Van <phamvvinh1998@gmail.com>
2021-10-05 12:35:09 +0100

    Add application icon

--- src/main/java/com/vinhderful/raytracer/RayTracer.java
+++ src/main/java/com/vinhderful/raytracer/RayTracer.java
@@ -6,6 +6,7 @@ import java.util.Objects;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -26,6 +27,7 @@ public class RayTracer extends Application {
             scene = new Scene(root);
 
         stage.setTitle("TornadoVM Ray Tracer");
+        stage.getIcons().add(new Image(Objects.requireNonNull(RayTracer.class.getResourceAsStream("icon.png"))));
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
