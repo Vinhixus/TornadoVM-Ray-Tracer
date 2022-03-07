0a944deab4ef07b79fe93f6ae0f04f0752484480
Vinh Pham Van <phamvvinh1998@gmail.com>
2022-03-07 00:32:31 +0000

    Update README.md, add code to render scene to png

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-## Java Ray Tracer for TornadoVM - W.I.P
+## Java Path Tracer for TornadoVM - W.I.P
 
 ![Demo](Demo.png)
 
 # Description
 
-This project aims to build a ray tracing engine in Java, accelerated on heterogeneous hardware using
+This project aims to build a path tracing engine in Java, accelerated on heterogeneous hardware using
 TornadoVM ([https://www.tornadovm.org/](https://www.tornadovm.org/)).
 
 The engine uses JavaFX for the graphical context and the graphical user interface.
--- src/main/java/com/vinhderful/raytracer/Controller.java
+++ src/main/java/com/vinhderful/raytracer/Controller.java
@@ -8,7 +8,15 @@ import com.vinhderful.raytracer.utils.Color;
 import com.vinhderful.raytracer.utils.Vector3f;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.image.WritableImage;
 import javafx.scene.layout.Pane;
+
+import javax.imageio.ImageIO;
+import java.awt.image.RenderedImage;
+import java.io.File;
+import java.io.IOException;
+
+import static javafx.embed.swing.SwingFXUtils.fromFXImage;
 
 /**
  * Initialises JavaFX FXML elements together with GUI.fxml, contains driver code
@@ -45,6 +53,28 @@ public class Controller {
         Cube cube = new Cube(new Vector3f(1.5F, 0, 2.5F), 2F, new Color(0.35F, 0.35F, 0.35F), 32F);
         world.addBody(cube);
 
+        // Render world on canvas
+        System.out.println("---------------------------------------");
+        System.out.println("Rendering scene...");
         renderer.render(world);
+        System.out.println("Scene rendered.");
+        System.out.println("---------------------------------------");
+
+        // Write result to file
+        System.out.println("Writing image to Render.png...");
+
+        File file = new File("Render.png");
+        try {
+            WritableImage writableImage = new WritableImage((int) canvas.getWidth(), (int) canvas.getHeight());
+            canvas.snapshot(null, writableImage);
+            RenderedImage renderedImage = fromFXImage(writableImage, null);
+            ImageIO.write(renderedImage, "png", file);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            System.out.println("Error!");
+        }
+
+        System.out.println("Done!");
+        System.out.println("---------------------------------------");
     }
 }
