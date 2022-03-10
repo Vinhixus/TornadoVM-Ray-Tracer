9cefe193d44c4e67a8e7401c44dc17bfa34a1b0f
Vinh Pham Van <phamvvinh1998@gmail.com>
2022-03-10 05:15:20 +0000

    Revamp benchmark

--- src/main/java/com/vinhderful/raytracer/Benchmark.java
+++ src/main/java/com/vinhderful/raytracer/Benchmark.java
@@ -2,17 +2,24 @@ package com.vinhderful.raytracer;
 
 import com.vinhderful.raytracer.misc.World;
 import com.vinhderful.raytracer.renderer.Renderer;
-import uk.ac.manchester.tornado.api.GridScheduler;
-import uk.ac.manchester.tornado.api.TaskSchedule;
-import uk.ac.manchester.tornado.api.WorkerGrid;
-import uk.ac.manchester.tornado.api.WorkerGrid2D;
+import uk.ac.manchester.tornado.api.*;
 import uk.ac.manchester.tornado.api.collections.types.VectorFloat;
 import uk.ac.manchester.tornado.api.collections.types.VectorFloat4;
+import uk.ac.manchester.tornado.api.common.TornadoDevice;
+import uk.ac.manchester.tornado.api.runtime.TornadoRuntime;
+
+import java.util.ArrayList;
+import java.util.Scanner;
 
 @SuppressWarnings("PrimitiveArrayArgumentToVarargsMethod")
 public class Benchmark {
 
-    public static final int TEST_LOOP_ITERATIONS = 10;
+    public static final int FRAMES_TO_GENERATE = 5;
+
+    public static final int WIDTH = 2048;
+    public static final int HEIGHT = 1024;
+    public static final int SHADOW_SAMPLE_SIZE = 200;
+    public static final int REFLECTION_BOUNCES = 4;
 
     private static int[] pixels;
     private static float[] camera;
@@ -21,14 +28,11 @@ public class Benchmark {
     // ==============================================================
 
     private static void setRenderingProperties() {
-        int width = 1920;
-        int height = 960;
-
-        dimensions = new int[]{width, height};
-        pixels = new int[width * height];
+        dimensions = new int[]{WIDTH, HEIGHT};
+        pixels = new int[WIDTH * HEIGHT];
 
         camera = new float[]{0, 0, -4F, 0, 0, 60};
-        pathTracingProperties = new int[]{18, 4};
+        pathTracingProperties = new int[]{SHADOW_SAMPLE_SIZE, REFLECTION_BOUNCES};
     }
 
     // ==============================================================
@@ -36,6 +40,8 @@ public class Benchmark {
 
         setRenderingProperties();
 
+        System.out.println("-----------------------------------------");
+        System.out.println("Building world...");
         World world = new World();
         VectorFloat4 skybox = world.getSkybox();
         int[] skyboxDimensions = world.getSkyboxDimensions();
@@ -52,20 +58,65 @@ public class Benchmark {
                 skybox, skyboxDimensions, pathTracingProperties);
         ts.streamOut(pixels);
 
-        WorkerGrid worker = new WorkerGrid2D(dimensions[0], dimensions[1]);
+        WorkerGrid worker = new WorkerGrid2D(WIDTH, HEIGHT);
         worker.setLocalWork(16, 16, 1);
         GridScheduler grid = new GridScheduler();
         grid.setWorkerGrid("s0.t0", worker);
         ts.execute(grid);
 
+        // Get Tornado devices
+        System.out.println("-----------------------------------------");
+        System.out.println("Getting Tornado Devices...");
+        ArrayList<TornadoDevice> devices = new ArrayList<>();
+        TornadoRuntimeCI runtimeCI = TornadoRuntime.getTornadoRuntime();
+        int numTornadoDrivers = runtimeCI.getNumDrivers();
+        int deviceCount = 0;
+
+        for (int i = 0; i < numTornadoDrivers; i++) {
+
+            TornadoDriver driver = runtimeCI.getDriver(i);
+            int numDevices = driver.getDeviceCount();
+
+            for (int j = 0; j < numDevices; j++) {
+                TornadoDevice device = driver.getDevice(j);
+                devices.add(device);
+
+                String listingName = "(" + driver.getName() + ") " + device.getPhysicalDevice().getDeviceName();
+                System.out.println(deviceCount + ": " + listingName);
+
+                ts.mapAllTo(device);
+                ts.execute(grid);
+                deviceCount++;
+            }
+        }
+
+        // Choose device
+        System.out.println("-----------------------------------------");
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        do {
+            System.out.print("Select device index: ");
+            input = scanner.nextLine();
+
+            if (!input.matches("^-?\\d+$"))
+                System.out.println("Please enter an integer!");
+            else if (Integer.parseInt(input) < 0 || Integer.parseInt(input) > deviceCount)
+                System.out.println("Please enter a valid index listed above!");
+            else
+                break;
+        } while (true);
+
+        ts.mapAllTo(devices.get(Integer.parseInt(input)));
+
         // ==============================================================
         // Run computation in parallel
         // ==============================================================
         System.out.println("-----------------------------------------");
-        System.out.println("Running test with TornadoVM...");
+        System.out.println("Generating " + FRAMES_TO_GENERATE + " frames with TornadoVM...");
         long startTime = System.nanoTime();
 
-        for (int i = 0; i < TEST_LOOP_ITERATIONS; i++)
+        for (int i = 0; i < FRAMES_TO_GENERATE; i++)
             ts.execute(grid);
 
         long endTime = System.nanoTime();
@@ -76,10 +127,10 @@ public class Benchmark {
         // Run computation sequentially
         // ==============================================================
         System.out.println("-----------------------------------------");
-        System.out.println("Running test sequentially...");
+        System.out.println("Generating " + FRAMES_TO_GENERATE + " frames sequentially...");
         startTime = System.nanoTime();
 
-        for (int i = 0; i < TEST_LOOP_ITERATIONS; i++)
+        for (int i = 0; i < FRAMES_TO_GENERATE; i++)
             Renderer.render(pixels, dimensions, camera,
                     bodyPositions, bodySizes, bodyColors, bodyReflectivities,
                     skybox, skyboxDimensions, pathTracingProperties);
