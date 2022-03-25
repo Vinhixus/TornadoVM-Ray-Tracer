877b667210e621d0bdca94ab894926bf2d3bed04
Vinh Pham Van <phamvvinh1998@gmail.com>
2022-03-25 00:53:26 +0000

    Remove square brackets from copyright information

--- src/main/java/com/vinhderful/raytracer/scene/Camera.java
+++ src/main/java/com/vinhderful/raytracer/scene/Camera.java
@@ -2,7 +2,7 @@
  * This file is part of Tornado-Ray-Tracer: A Java-based ray tracer running on TornadoVM.
  * URL: https://github.com/Vinhixus/TornadoVM-Ray-Tracer
  *
- * Copyright [2022] [Vinh Pham Van]
+ * Copyright (c) 2021-2022, Vinh Pham Van
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
